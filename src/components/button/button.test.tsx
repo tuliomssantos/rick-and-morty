9966b3fa0436b34717467cf6902da47aebf1da15
Tulio <tuliomssantos@gmail.com>
2022-03-03 14:43:58 -0300

    refactor: remove demo button

--- src/components/button/button.test.tsx
+++
@@ -1,8 +0,0 @@
-import Button from './'
-
-import { renderWithTheme } from '@/src/utils'
-
-it('renders Button unchanged', () => {
-  const { container } = renderWithTheme(<Button />)
-  expect(container.firstChild).toMatchSnapshot()
-})
